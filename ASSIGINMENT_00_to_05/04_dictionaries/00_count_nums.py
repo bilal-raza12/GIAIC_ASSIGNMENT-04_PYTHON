6694200f2e58225ceb1658f3ea07f6e997d1b1da
bilal-raza12 <160725888+bilal-raza12@users.noreply.github.com>
2025-04-05 01:44:17 +0500

    04_dictionaries

---
+++ ASSIGINMENT_00_to_05/04_dictionaries/00_count_nums.py
@@ -0,0 +1,33 @@
+def get_numbers():
+    num_list: list = []
+    while True:
+        num = input("Enter a number: ")
+        if num == "":
+            break
+
+        num = int(num)
+        num_list.append(num)
+    return num_list
+
+
+def num_counts(num_list):
+    
+    num_dict: dict = {}
+    for i in num_list:
+        if i in num_dict:
+            num_dict[i] += 1
+        else:
+            num_dict[i] = 1
+    return num_dict
+
+def print_counts(num_dict):
+    for num in num_dict:
+        print(f"{str(num)} appears {str(num_dict[num])} times.")
+def main():
+    user_input = get_numbers()
+    num_dict = num_counts(user_input)
+    print_counts(num_dict)
+
+if __name__ == "__main__":
+    main()
+
